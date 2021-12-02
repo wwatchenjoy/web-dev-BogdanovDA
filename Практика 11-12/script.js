1df568299b371c0e51d3956731f5e436674f4a3c
Dima <test>
2021-12-02 12:26:52 +0300

    Все задания.

--- Практика 11-12/script.js
+++ Практика 11-12/script.js
@@ -56,3 +56,67 @@ var obj = {
   }
 
 console.log(obj.method1().method2().method3());
+
+
+
+
+function Card(from, to){
+    this.from = from;
+    this.to = to;
+};
+
+var c1 = new Card("Екатеринбург ","Москва");
+console.log(c1);
+
+
+
+class Human {
+    constructor(name, age, height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+    /**
+     * Информация о человеке вида «Коля, 23, 180»
+     * @return {string}
+     */
+    getInfo() {
+        return `${this.name}, ${this.age}, ${this.height}`;
+    }
+    /**
+     * Имя человека
+     * @return {string}
+     */
+    get firstname() {
+        return this.name;
+    }
+
+    toString(){
+        return `{name: "${this.name}", age: ${this.age}, height:${this.height}}`;
+    };
+
+    
+}
+
+
+let humans = [
+    new Human("Коля", 23, 180),
+    new Human("Даша", 19, 170),
+    new Human("Ваня", 18, 192),
+    new Human("Петя", 45, 178),
+    new Human("Вася", 34, 197),
+    new Human("Джони", 40, 168),
+    new Human("Катя", 37, 160),
+    new Human("Петя", 29, 200),
+    new Human("Соня", 21, 172),
+    new Human("Женя", 25, 175)
+];
+
+let json = JSON.stringify(humans);
+console.log(json);
+
+for (let human of humans)
+    console.log(human.getInfo());
+
+
+
